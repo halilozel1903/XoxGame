0d4d56050c2a59e210594e39c02798c412582dfc
Halil Ozel <halilozel1903@gmail.com>
2025-06-22 18:37:40 +0300

    refactor(ui): Move color and constant values to UiConstants object

---
+++ app/src/main/java/com/halil/ozel/xoxgame/presentation/ui/UiConstants.kt
@@ -0,0 +1,17 @@
+package com.halil.ozel.xoxgame.presentation.ui
+
+/**
+ * Created by halilozel1903 on 22.06.2025.
+ */
+import androidx.compose.ui.graphics.Color
+
+object UiConstants {
+    val BoardBackground = Color(0xFFFAFAFA)
+    val CellBackground = Color(0xFFE0E0E0)
+    val TextDark = Color(0xFF222222)
+    val TextBlue = Color(0xFF1976D2)
+    val TextRed = Color(0xFFD32F2F)
+    val TextGreen = Color(0xFF43A047)
+    val TextOrange = Color(0xFFFB8C00)
+    val TextGray = Color(0xFF616161)
+}
